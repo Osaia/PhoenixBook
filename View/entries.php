eb2fbb222dae476b4e0b6a0ffe32d9d1b051902a
Janik1234 <bindej@bbcnet.ch>
2016-03-08 15:22:04 +0100

    entries
    css

--- View/entries.php
+++ View/entries.php
@@ -1,13 +1,17 @@
- <div style="background-color: lightseagreen" class="col-xs-9 col-ld-9">
-    <a href="#">
-        <div class="entry">
-            <p>name der person</p>
-        </div>
-    </a>
- </div>
-    <div style="background-color: dodgerblue" class="col-xs-3 col-ld-3">
+ <div class="col-xs-9 col-ld-9">
+     <div class="col-xs-9 col-ld-9 col-md-offset-1" id="entries-container">
+        <a href="#">
+            <div class="entries">
+                <p>name der person</p>
+                <div class="entries-footer">
 
-    </div>
+                </div>
+            </div>
+        </a>
+     </div>
+ </div>
+<div class="col-xs-3 col-ld-3">
+
 </div>
 <?php
 
