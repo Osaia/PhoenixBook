4b676b35ce017853f80b559bbb305a3c49422f06
Janik1234 <bindej@bbcnet.ch>
2016-03-08 14:20:10 +0100

    entries
    View entries.php: Bildschirmaufteilung

--- Controller/entriesController.php
+++ Controller/entriesController.php
@@ -1,1 +1,15 @@
 <?php
+
+class entriesController
+{
+    public function index()
+    {
+        // $userModel = new UserModel();
+
+        $view = new View('entries');
+        $view->title = 'Entries';
+        $view->heading = 'Entries';
+        // $view->users = $userModel->readAll();
+        $view->display();
+    }
+}
--- View/entries.php
+++ View/entries.php
@@ -1,9 +1,14 @@
-<body>
-    <div id="container">
-        
-    </div>
-</body>
+ <div style="background-color: lightseagreen" class="col-xs-9 col-ld-9">
+    <a href="#">
+        <div class="entry">
+            <p>name der person</p>
+        </div>
+    </a>
+ </div>
+    <div style="background-color: dodgerblue" class="col-xs-3 col-ld-3">
 
+    </div>
+</div>
 <?php
 
 ?>
