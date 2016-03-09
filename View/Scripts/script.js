52e3e99c8bb1ef792bc2778ac53d9c1759e0c818
Osaia <jessicadominguez95@gmail.com>
2016-03-09 13:10:03 +0100

    Validation
    Validation funktioniert in registrierungsform. Es fehlt jedoch noch eine
    richtige anzeige (mehr als ein Alert)  von dem was funktioniert

--- View/Scripts/script.js
+++ View/Scripts/script.js
@@ -33,32 +33,65 @@ var loginForm = {
 var RegisForm = {
     //Properties
     patEmail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    //A regexp for general username entry. Which doesn't allow special characters ' +
+    //'other than underscore. ' +    'Username must be of length ranging(3-30). ' +
+    //'starting letter should be a ' +
+    //'number or a character.
+    patUsername: /^[a-zA-Z0-9][a-zA-Z0-9_]{5,29}$/,
+    patName: /^[a-zA-Z0-9]{5,20}$/,
+    //Password expresion that requires one lower case letter,
+    //one upper case letter, one digit, 6-13 length, and no spaces.
+    patPwd: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,8}$/,
+
 
     init: function(){
-        $('#submitRegis').click(function(){
-            alert('Regis Submit');
+        $('#registerform').submit(function(event){
+            if(RegisForm.finalValidation()){
+                alert('allright');
+                event.preventDefault();
+            }else{
+                event.preventDefault();
+            }
         });
 
         $('#firstNameRegis').focus(function(){
-            alert('in');
+
         }).blur(function(){
-            alert('out');
+            if(RegisForm.patName.test(this.value) == true){
+                this.className = 'form-control input-md';
+                $(this).addClass('onSuccess');
+            }else{
+                this.className = 'form-control input-md';
+                $(this).addClass('onError');
+            }
         });
 
         $('#lastNameRegis').focus(function(){
-            alert('in');
+
         }).blur(function () {
-            alert('out');
+            if(RegisForm.patName.test(this.value) == true){
+                this.className = 'form-control input-md';
+                $(this).addClass('onSuccess');
+            }else{
+                this.className = 'form-control input-md';
+                $(this).addClass('onError');
+            }
         });
 
-        //$('#user').focus(function(){
-        //    alert('in');
-        //}).blur(function(){
-        //    alert('out');
-        //});
+        $('#usernameRegis').focus(function(){
+            //alert('in');
+        }).blur(function(){
+            if(RegisForm.patUsername.test(this.value) == true){
+                this.className = 'form-control input-md';
+                $(this).addClass('onSuccess');
+            }else{
+                this.className = 'form-control input-md';
+                $(this).addClass('onError');
+            }
+        });
 
         $('#emailRegis').focus(function () {
-            alert('in');
+
         }).blur(function(){
             if(RegisForm.patEmail.test(this.value) == true){
                 this.className = 'form-control input-md';
@@ -70,18 +103,65 @@ var RegisForm = {
         });
 
         $('#passwordRegis').focus(function(){
-            alert('in');
+            $('#password2Regis').val('');
+            var t = 0;
         }).blur(function(){
-            alert('out');
+            if(RegisForm.patPwd.test(this.value) == true){
+                this.className = 'form-control input-md';
+                $(this).addClass('onSuccess');
+            }else{
+                this.className = 'form-control input-md';
+                $(this).addClass('onError');
+            }
         });
 
         $('#password2Regis').focus(function () {
-            alert('in');
-        }).blur(function(){
-            alert('out');
-        });
 
+        }).blur(function(){
+            var tm = 0;
+            if($('#passwordRegis').val() == this.value){
+                this.className = 'form-control input-md';
+                $(this).addClass('onSuccess');
+            }else{
+                this.className = 'form-control input-md';
+                $(this).addClass('onError');
+            }
+        });
         //FILE UPLOAD ??
+    },
+
+    finalValidation: function(){
+        if(RegisForm.patUsername.test($('#usernameRegis').val())){
+            if(RegisForm.patName.test($('#firstNameRegis').val())){
+                if(RegisForm.patName.test($('#lastNameRegis').val())){
+                    if(RegisForm.patEmail.test($('#emailRegis').val())){
+                        if(RegisForm.patPwd.test($('#passwordRegis').val())){
+                            if($('#passwordRegis').val() == $('#password2Regis').val()){
+                                return true;
+                            }else{
+                                alert('Niggha.. Those passwords are not equal... CANT YOU SEE? ...Oh right.. You cant, my bad..*LMFAO* Try again...');
+                                return false;
+                            }
+                        }else{
+                            alert('No passwords like that admited... No way!');
+                            return false;
+                        }
+                    }else{
+                        alert('Oh hell no thats no mail adress D:');
+                        return false;
+                    }
+                }else{
+                    alert('That should be your last name?... Try again');
+                    return false;
+                }
+            }else{
+                alert('That should be your first name?... Try again');
+                return false;
+            }
+        }else{
+            alert('Not a valid Username');
+            return false;
+        }
     }
 }
 
