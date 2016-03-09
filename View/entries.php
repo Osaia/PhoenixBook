bfeac21a4939711282360083fbe7555983d3e0a9
Janik1234 <bindej@bbcnet.ch>
2016-03-09 09:14:35 +0100

    entries
    Layout gestaltung. Warte auf einbindung von script.js

--- View/entries.php
+++ View/entries.php
@@ -1,20 +1,34 @@
 
 
     <div ID="entriesWrapper" class="col-xs-12 col-ld-12">
-        <div class="col-xs-9 col-ld-9">
-             <div class="col-xs-9 col-ld-9 col-md-offset-1" id="entries-container">
-                <a href="#">
-                    <div class="entries">
-                        <p>name der person</p>
-                        <div class="entries-footer">
+        <div id="entries-background" class="col-xs-12 col-ld-12">
+            <div class="col-xs-9 col-ld-9">
+                 <div class="col-xs-9 col-ld-9 col-md-offset-1" id="entries-container">
+                    <a href="#">
+                        <div class="entries">
+                            <p>name der person</p>
+                            <div class="entries-txt">
+                                <p></p>
+                            </div>
+                            <div class="entries-footer">
 
+                            </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                 </div>
              </div>
-         </div>
-        <div class="col-xs-3 col-ld-3">
-
+            <div class="col-xs-3 col-ld-3 sidebar-top">
+                <div id="sidebar-big" class="col-xs-10 col-ld-10">
+                    <button type="button" class="btn btn-default" aria-label="Left Align">
+                        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div id="sidebar-small" class="col-xs-10 col-ld-10">
+                    <button type="button" class="btn btn-default" aria-label="Left Align">
+                        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 <?php
